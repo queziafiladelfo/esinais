948477aeacd36093bb06631695f4b55d5b4b45d9
Quézia Filadelfo <queziafiladelfo@gmail.com>
2024-09-27 07:00:07 -0300

    deploy 3

--- src/config/configApi.js
+++ src/config/configApi.js
@@ -2,5 +2,6 @@ import axios from "axios";
 
 
 export default axios.create({
-    baseURL:'http://localhost:8080'
+    // baseURL:'http://localhost:8080'
+    baseURL:'http://157.230.14.156:8080/'
 })
